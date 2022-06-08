afa61cabd10f04c8f1984322062730ef119c97fb
Rakesh Bodavula <rakeshbodavula2002@gmail.com>
2022-06-08 08:54:25 +0530

    changed dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const path = require('path')
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const mongoose = require('mongoose')
 const User = require('./model/user')
 const Crop = require('./model/crop')
